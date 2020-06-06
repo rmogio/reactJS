57b680ed0f4ca9c33ff8e2dbef9151e8dd2f9291
Ricardo Mogio <ricardo.mogio@gmail.com>
2020-06-06 12:43:16 -0300

    Adicionado arquivos de production

---
+++ projeto_react_alura/build/precache-manifest.37c91f82e56aa7e3d20f56c4971ebe6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e64fc03321e3e5e4dbd850e4ba6b116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "724348998d3640fabb1c",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "44dc7a1df33a2c454a0d",
+    "url": "/static/css/main.2e25fab5.chunk.css"
+  },
+  {
+    "revision": "724348998d3640fabb1c",
+    "url": "/static/js/2.0484e030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0484e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44dc7a1df33a2c454a0d",
+    "url": "/static/js/main.7b491a6d.chunk.js"
+  },
+  {
+    "revision": "15bc7bbb5b185e904385",
+    "url": "/static/js/runtime-main.7c377078.js"
+  }
+]);
